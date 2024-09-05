e1c86ead0f614421cc60ce7f1781aa617b47c5aa
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-09-05 10:26:39 +0530

    INFINITE SCROLL implemented

--- INFINITE-SCROLL/app.js
+++ INFINITE-SCROLL/app.js
@@ -12,7 +12,7 @@ const imageContainer = document.getElementById("imagecontainer")
 async function getImages() {
     const response = await fetch("https://api.unsplash.com/photos/?client_id=Jhg45wbjlt-9FIA8B3hzHueL9oKAmiKb7j8c5aBg63Y")
     const imagesData = await response.json()
-    console.log(imagesData)
+    // console.log(imagesData)
 
     imagesData.map(function(i){
         let imageURL = i.urls.full
@@ -26,3 +26,24 @@ async function getImages() {
 }
 
 getImages()
+
+// window object refers the entire window of the browser.
+window.addEventListener("scroll", function(){
+
+    // console.log("hi")
+    
+    // const data = document.documentElement // gives the complete details of the html elements.
+    // console.log(data)
+
+    let myClientHeight = document.documentElement.clientHeight  // Height of the screen that we see
+    let myScrollHeight = document.documentElement.scrollHeight  // total height including scroll
+    let myScrollTop = document.documentElement.scrollTop        // distance moved on scrolling 
+
+    // console.log(myClientHeight, myScrollHeight, myScrollTop)
+
+    if(myClientHeight + myScrollTop >= myScrollHeight){
+
+        getImages()
+    }
+
+})
