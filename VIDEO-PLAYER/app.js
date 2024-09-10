06c0d9028c3356a6c64710e8565ab39f3a1e21de
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-09-10 11:02:02 +0530

    Video timing done

--- VIDEO-PLAYER/app.js
+++ VIDEO-PLAYER/app.js
@@ -1,6 +1,8 @@
 const PlayButton = document.getElementById("play")
 const myVideo = document.querySelector("video")
 const progressBar = document.getElementById("progress-bar")
+const videoDuration = document.getElementById("duration-time")
+const videoTime = document.getElementById("current-time")
 
 
 let isVideoPlaying = false
@@ -31,9 +33,37 @@ PlayButton.addEventListener("click", controlVideo)
 myVideo.addEventListener("timeupdate", function(event){
     // console.log(event) // currentTime, duration
     let myCurrentTime = myVideo.currentTime
-    let myduration = myVideo.duration
+    let myDuration = myVideo.duration
     // console.log(myCurrentTime, myduration)
-    let progressPercentage = (myCurrentTime / myduration) * 100
+    let progressPercentage = (myCurrentTime / myDuration) * 100
     progressBar.style.width = `${progressPercentage}%`
+
+    // Logic for duration --->  Minutes & seconds
+
+    const durationInMinutes = Math.floor(myDuration / 60)
+    // console.log(durationInMinutes)
+    let durationInSeconds = Math.floor(myDuration % 60)
+    // console.log(durationInSeconds)
+
+    if( durationInSeconds <= 9){
+        durationInSeconds = `0${durationInSeconds}`
+    }
+
+    videoDuration.innerText = `${durationInMinutes}:${durationInSeconds}`
+
+    // Logic for current time 
+
+    const currentTimeInMinutes = Math.floor(myCurrentTime /60)
+    // console.log(currentTimeInMinutes)
+
+    let currentTimeInSeconds = Math.floor(myCurrentTime % 60)
+    // console.log(currentTimeInSeconds)
+
+    if(currentTimeInSeconds <= 9){
+        currentTimeInSeconds = `0${currentTimeInSeconds}`
+    }
+
+    videoTime.innerText = `${currentTimeInMinutes}:${currentTimeInSeconds}/`
 })
+
 
