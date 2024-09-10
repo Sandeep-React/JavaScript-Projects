d0a6bca69359d4e6c578b162e9de052be71b3dde
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-09-10 11:23:23 +0530

    Video timing code optimised

--- VIDEO-PLAYER/app.js
+++ VIDEO-PLAYER/app.js
@@ -30,40 +30,56 @@ function controlVideo(){
 PlayButton.addEventListener("click", controlVideo)
 
 
+// myVideo.addEventListener("timeupdate", function(event){
+//     // console.log(event) // currentTime, duration
+//     let myCurrentTime = myVideo.currentTime
+//     let myDuration = myVideo.duration
+//     // console.log(myCurrentTime, myduration)
+//     let progressPercentage = (myCurrentTime / myDuration) * 100
+//     progressBar.style.width = `${progressPercentage}%`
+
+//     // Logic for duration --->  Minutes & seconds
+
+//     const durationInMinutes = Math.floor(myDuration / 60)
+//     // console.log(durationInMinutes)
+//     let durationInSeconds = Math.floor(myDuration % 60)
+//     // console.log(durationInSeconds)
+
+//     if( durationInSeconds <= 9){
+//         durationInSeconds = `0${durationInSeconds}`
+//     }
+
+//     videoDuration.innerText = `${durationInMinutes}:${durationInSeconds}`
+
+//     // Logic for current time 
+
+//     const currentTimeInMinutes = Math.floor(myCurrentTime /60)
+//     // console.log(currentTimeInMinutes)
+
+//     let currentTimeInSeconds = Math.floor(myCurrentTime % 60)
+//     // console.log(currentTimeInSeconds)
+
+//     if(currentTimeInSeconds <= 9){
+//         currentTimeInSeconds = `0${currentTimeInSeconds}`
+//     }
+
+//     videoTime.innerText = `${currentTimeInMinutes}:${currentTimeInSeconds}/`
+// })
+
+
+function formateTime(seconds){
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = Math.floor(seconds % 60)
+    return `${minutes}:${remainingSeconds < 10 ? '0': ''}${remainingSeconds}`
+}
+
 myVideo.addEventListener("timeupdate", function(event){
-    // console.log(event) // currentTime, duration
-    let myCurrentTime = myVideo.currentTime
-    let myDuration = myVideo.duration
-    // console.log(myCurrentTime, myduration)
-    let progressPercentage = (myCurrentTime / myDuration) * 100
+    const myCurrentTime = myVideo.currentTime
+    const myDuration = myVideo.duration
+
+    const progressPercentage = (myCurrentTime / myDuration) * 100;
     progressBar.style.width = `${progressPercentage}%`
 
-    // Logic for duration --->  Minutes & seconds
-
-    const durationInMinutes = Math.floor(myDuration / 60)
-    // console.log(durationInMinutes)
-    let durationInSeconds = Math.floor(myDuration % 60)
-    // console.log(durationInSeconds)
-
-    if( durationInSeconds <= 9){
-        durationInSeconds = `0${durationInSeconds}`
-    }
-
-    videoDuration.innerText = `${durationInMinutes}:${durationInSeconds}`
-
-    // Logic for current time 
-
-    const currentTimeInMinutes = Math.floor(myCurrentTime /60)
-    // console.log(currentTimeInMinutes)
-
-    let currentTimeInSeconds = Math.floor(myCurrentTime % 60)
-    // console.log(currentTimeInSeconds)
-
-    if(currentTimeInSeconds <= 9){
-        currentTimeInSeconds = `0${currentTimeInSeconds}`
-    }
-
-    videoTime.innerText = `${currentTimeInMinutes}:${currentTimeInSeconds}/`
+    videoDuration.innerText = formateTime(myDuration)
+    videoTime.innerText = `${formateTime(myCurrentTime)}/`
 })
-
-
