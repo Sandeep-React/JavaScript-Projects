a57974bd5900fd08582ce83a3281d20234a4f8e7
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-09-16 16:40:00 +0530

    Data fetched in the right container

--- FOOD BUZZ/app.js
+++ FOOD BUZZ/app.js
@@ -12,9 +12,10 @@ API key
 const searchBtn = document.getElementById("search")
 const searchInput = document.getElementById("searchinput")
 const leftContainer = document.getElementById("left-container")
+const rightContainer = document.getElementById("right-container")
 
 searchBtn.addEventListener("click", ()=>{
-    getRecipesData()
+    // getRecipesData()
 })
 
 async function getRecipesData()
@@ -30,13 +31,16 @@ async function getRecipesData()
     const myImageUrl =i.image_url
     const myPublisher = i.publisher
     const myTitle = i.title
+    const myId = i.id
         return leftContainer.insertAdjacentHTML("afterbegin",
-        `<div class="left-food-container">
+
+            `<a href="#"><div class="left-food-container">
         <img src="${myImageUrl}" id="myimage">
         <h2 id = "mypublisher">${myPublisher}</h2>
         <h3 id ="mytitle">${myTitle}</h3>
-       </div>`)
-
+       </div></a>`
+        
+        )
     })
     }
     catch(e){
@@ -44,4 +48,38 @@ async function getRecipesData()
     }
 }
 
-getRecipesData()
+// getRecipesData()
+
+async function loadParticularRecipe(){
+    const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8357")
+    const recipeData = await response.json()
+    console.log(recipeData)
+
+    const recipeObject = {
+        publisher: recipeData.data.recipe.publisher,
+        imageUrl: recipeData.data.recipe.image_url,
+        title: recipeData.data.recipe.title,
+        servings: recipeData.data.recipe.servings,
+        cookingTime: recipeData.data.recipe.cooking_time,
+        ingredients: recipeData.data.recipe.ingredients
+    }
+
+    // console.log(recipeObject)
+
+    rightContainer.innerText =""
+
+   const rightData = `<div class="right-food-container">
+    <img src="${recipeObject.imageUrl}" alt="">
+    <h2>${recipeObject.title}</h2>
+    <h3>${recipeObject.publisher}</h3>
+    <h3>${recipeObject.servings}</h3>
+    <h3>${recipeObject.cookingTime}</h3>
+</div>`
+
+    rightContainer.insertAdjacentHTML("afterbegin", rightData)
+    
+}
+
+loadParticularRecipe()
+
+// https://forkify-api.herokuapp.com/api/v2/recipes/:664c8f193e7aa067e94e8706
