7723ceb628d7d773a519168a3ccc55c485af38d9
Sandeep Kumar Das <sandeepkrdas24@gmail.com>
2024-09-18 09:51:00 +0530

    MVC architecture added for right container

---
+++ FOOD BUZZ/MVC/OneRecipeView.js
@@ -0,0 +1,53 @@
+import { anotherRecipeObject } from "./MyModel.js";
+
+
+export class OneRecipeView{
+
+  rightContainer;
+  rightData;
+
+  render()
+  {
+     this.rightContainer = document.getElementById("right-container")
+     this.clear()
+    //  console.log(anotherRecipeObject.recipeObject)
+     let collectedData = anotherRecipeObject.recipeObject
+     this.rightData = this.actualLogic(collectedData)
+     this.addDataToContainer()
+    }
+    
+    clear(){
+    this.rightContainer.innerText =""
+
+  }
+
+  actualLogic(RecievedData){
+    // console.log(RecievedData)
+    return `<div class="right-food-container">
+     <img class = "right-image" src="${RecievedData.imageUrl}" alt="">
+     <h2 class = "right-title">Title: ${RecievedData.title}</h2>
+     <h3 class = "right-publisher">Publisher: ${RecievedData.publisher}</h3>
+     <h3 class = "right-servings">Servings: ${RecievedData.servings}</h3>
+     <h3 class = "right-cooking-time">Cooking Time: ${RecievedData.cookingTime}</h3>
+ 
+     <div class = "ingredients">
+       ${RecievedData.ingredients.map(function(i){
+         // console.log(i)
+             return `<div>
+                 <span>${i.description}</span> --
+                 <span>${i.quantity}</span>
+                 <span>${i.unit}</span>
+             </div>`
+       }).join("")}
+     </div>
+ 
+ </div>`
+  }
+
+  addDataToContainer(){
+    this.rightContainer.insertAdjacentHTML("afterbegin", this.rightData)
+  }
+    
+ 
+
+}
---
+++ FOOD BUZZ/controller.js
@@ -0,0 +1,67 @@
+// https://forkify-api.herokuapp.com/v2
+
+//API key
+//9c212eec-a3fe-4d83-bfe5-b69768886469
+
+
+
+import { storeReciepeData } from "./MVC/MyModel.js "
+import { OneRecipeView } from "./MVC/OneRecipeView.js"
+
+
+const searchBtn = document.getElementById("search")
+const searchInput = document.getElementById("searchinput")
+const leftContainer = document.getElementById("left-container")
+// const rightContainer = document.getElementById("right-container")
+
+searchBtn.addEventListener("click", ()=>{
+    getRecipesData()
+})
+
+async function getRecipesData()
+{
+    try{
+    const searchItem = searchInput.value
+    const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchItem}&key=9c212eec-a3fe-4d83-bfe5-b69768886469`)
+    const recipesData = await response.json()
+    const recpieArray = recipesData.data.recipes
+    recpieArray.map(function(i){
+    //   console.log(i)
+   
+    const myImageUrl =i.image_url
+    const myPublisher = i.publisher
+    const myTitle = i.title
+    const myId = i.id
+        return leftContainer.insertAdjacentHTML("afterbegin",
+
+            `<a href="#${myId}"><div class="left-food-container">
+        <img src="${myImageUrl}" id="myimage">
+        <h2 id = "mypublisher">${myPublisher}</h2>
+        <h3 id ="mytitle">${myTitle}</h3>
+       </div></a>`
+        
+        )
+    })
+    }
+    catch(e){
+        alert(e)
+    }
+}
+
+// getRecipesData()
+
+async function loadParticularRecipe(){
+    const hashId = window.location.hash.slice(1)
+    console.log(hashId)
+   
+   await storeReciepeData(hashId)
+
+   const rv = new OneRecipeView()
+   rv.render()
+}
+
+loadParticularRecipe()
+
+window.addEventListener("hashchange", loadParticularRecipe)
+
+// https://forkify-api.herokuapp.com/api/v2/recipes/:664c8f193e7aa067e94e8706
