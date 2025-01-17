f0fa019c8b9f6f1b8be28f830d745292fd5a1dd3
Sandeep-React <sandeepkrdas24@gmail.com>
2025-01-17 15:14:25 +0530

    servering funtionality added

--- FOOD BUZZ/MVC/ServingsView.js
+++ FOOD BUZZ/MVC/ServingsView.js
@@ -7,19 +7,86 @@ export class ServingsView
         this.rightContainer = document.getElementById("right-container")
         this.handleServings()
     }
-
+    
     handleServings(){
         this.rightContainer.addEventListener("click", function(e){
+        this.rightContainer = document.getElementById("right-container")
 
             // console.log(e.target.innerText)
             const btnName = e.target.innerText
 
             if(btnName == "INCREASE"){
-                console.log(anotherRecipeObject.recipeObject)
-            }
+                const completeDate = anotherRecipeObject.recipeObject
+                let myServings = completeDate.servings
+                anotherRecipeObject.recipeObject.servings = myServings + 1
+                // console.log(anotherRecipeObject.recipeObject.servings)
+
+                anotherRecipeObject.recipeObject.ingredients.map(function(i){
+                    i.quantity = Math.ceil((i.quantity * anotherRecipeObject.recipeObject.servings) / 5)
+                })
+
+                
+                const RecievedData = anotherRecipeObject.recipeObject
+                this.rightContainer.innerText=""
+                return this.rightContainer.insertAdjacentHTML("afterbegin", `<div class="right-food-container">
+                <img class = "right-image" src="${RecievedData.imageUrl}" alt="">
+                <h2 class = "right-title">Title: ${RecievedData.title}</h2>
+                <h3 class = "right-publisher">Publisher: ${RecievedData.publisher}</h3>
+                <h3 class = "right-servings">Servings: ${RecievedData.servings}</h3>
+                <button id = "inc">INCREASE</button>
+                <button id = "dec">DECREASE</button>
+                <h3 class = "right-cooking-time">Cooking Time: ${RecievedData.cookingTime}</h3>
+            
+                <div class = "ingredients">
+                  ${RecievedData.ingredients.map(function(i){
+                    // console.log(i)
+                        return `<div>
+                            <span>${i.description}</span> --
+                            <span>${i.quantity}</span>
+                            <span>${i.unit}</span>
+                        </div>`
+                  }).join("")}
+                </div>
+            
+            </div>`
+        )}
             else if(btnName == "DECREASE"){
                 
-            }
+                const completeDate = anotherRecipeObject.recipeObject
+                let myServings = completeDate.servings
+                anotherRecipeObject.recipeObject.servings = myServings - 1
+                // console.log(anotherRecipeObject.recipeObject.servings)
+
+                anotherRecipeObject.recipeObject.ingredients.map(function(i){
+                    i.quantity = Math.ceil((i.quantity * anotherRecipeObject.recipeObject.servings) / 5)
+                })
+
+                
+                const RecievedData = anotherRecipeObject.recipeObject
+                this.rightContainer.innerText=""
+                return this.rightContainer.insertAdjacentHTML("afterbegin", `<div class="right-food-container">
+                <img class = "right-image" src="${RecievedData.imageUrl}" alt="">
+                <h2 class = "right-title">Title: ${RecievedData.title}</h2>
+                <h3 class = "right-publisher">Publisher: ${RecievedData.publisher}</h3>
+                <h3 class = "right-servings">Servings: ${RecievedData.servings}</h3>
+                <button id = "inc">INCREASE</button>
+                <button id = "dec">DECREASE</button>
+                <h3 class = "right-cooking-time">Cooking Time: ${RecievedData.cookingTime}</h3>
+            
+                <div class = "ingredients">
+                  ${RecievedData.ingredients.map(function(i){
+                    // console.log(i)
+                        return `<div>
+                            <span>${i.description}</span> --
+                            <span>${i.quantity}</span>
+                            <span>${i.unit}</span>
+                        </div>`
+                  }).join("")}
+                </div>
+            
+            </div>`
+                
+        )}
 
         })
     }
